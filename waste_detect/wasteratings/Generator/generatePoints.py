94a0b04b04a59595d4df969a989f1365ff6d66a1
Ray Liu <rayliu109@gmail.com>
2018-05-26 15:53:30 -0400

    Finished generating points

--- waste_detect/wasteratings/Generator/generatePoints.py
+++ waste_detect/wasteratings/Generator/generatePoints.py
@@ -1,6 +1,8 @@
 from wasteratings.models import Rating
 from datetime import datetime,timedelta
 import random
+from django.utils import timezone
+
 def points():
 
 	longhi, longlo, lathi, latlo = -79.313432,-79.437372 ,43.752594,43.634184
@@ -13,7 +15,7 @@ def points():
 			long = longlo + random.random()*dlong
 			rating = round(max(1, min(random.gauss(day/20 + 1 , 0.75), 5)))
 
-			newrating = Rating(longitude = long, latitude= lat, rating = rating, date = datetime.now()-timedelta(days=100-day))
+			newrating = Rating(longitude = long, latitude= lat, rating = rating, date = timezone.now()-timedelta(days=100-day))
 			newrating.save()
 
 
