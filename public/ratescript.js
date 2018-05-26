4ec76f787e353b2d1a10f472b625f2f50d9c4961
Ar1sD <lwinn16@yahoo.ca>
2018-05-26 13:03:43 -0400

    Merge popup.html into rate.html
    - Merge and remove popup.html

--- public/ratescript.js
+++ public/ratescript.js
@@ -3,39 +3,58 @@ function initRateMap() {
         center: {lat: 43.6532, lng: -79.3832},
         zoom: 15
     });
+
+    document.getElementById("rate-btn").addEventListener("click", getLocation);
 }
 
 function getLocation() {
-//     // Try HTML5 geolocation.
-//     if (navigator.geolocation) {
-//         navigator.geolocation.getCurrentPosition(function(position) {
-//             var pos = {
-//                 lat: position.coords.latitude,
-//                 lng: position.coords.longitude
-//             };
-//
-//             infoWindow.setPosition(pos);
-//             infoWindow.setContent('Location found.');
-//             infoWindow.open(map);
-//             map.setCenter(pos);
-//         }, function() {
-//             handleLocationError(true, infoWindow, map.getCenter());
-//         });
-//     } else {
-//         // Browser doesn't support Geolocation
-//         handleLocationError(false, infoWindow, map.getCenter());
-//     }
-// }
-    here = "X";
-    alert("You are at: " + here);
+    infoWindow = new google.maps.InfoWindow;
+
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('Location: ' + pos.lat + "," + pos.lng);
+            infoWindow.open(map);
+            map.setCenter(pos);
+
+            addRating(pos)
+
+        }, function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+        });
+    } else {
+        // Browser doesn't support Geolocation
+        handleLocationError(false, infoWindow, map.getCenter());
+    }
+
+    //TODO: DO the thing where you close the marker on the screen if more than one click.
 }
 
-// function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-//     infoWindow.setPosition(pos);
-//     infoWindow.setContent(browserHasGeolocation ?
-//         'Error: The Geolocation service failed.' :
-//         'Error: Your browser doesn\'t support geolocation.');
-//     infoWindow.open(map);
-// }
+function addRating(pos) {
+    alert(pos.lat + "," + pos.lng);
+
+
+    var text = '{ ' +
+        '"employees" : [' +
+        '{ "firstName":"John" , "lastName":"Doe" },' +
+        '{ "firstName":"Anna" , "lastName":"Smith" },' +
+        '{ "firstName":"Peter" , "lastName":"Jones" } ]}';
+
+    var obj = JSON.parse(text);
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ?
+        'Error: The Geolocation service failed.' :
+        'Error: Your browser doesn\'t support geolocation.');
+    infoWindow.open(this.map);
+}
 
 initRateMap()
