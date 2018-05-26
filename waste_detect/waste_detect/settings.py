cc52435c78b99fb959a4d9869a6c5740f0e7ceb8
Ray Liu <rayliu109@gmail.com>
2018-05-26 01:39:12 -0400

    added apps

--- waste_detect/waste_detect/settings.py
+++ waste_detect/waste_detect/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'waste_detect.apps.WasteConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
