8b6d2cc7115a76fdab68b40c3fa227af9705c5a3
Ray Liu <rayliu109@gmail.com>
2018-05-26 13:04:21 -0400

    Finihsed random point generator

---
+++ waste_detect/wasteratings/Generator/generatePoints.py
@@ -0,0 +1,22 @@
+from wasteratings.models import Rating
+from datetime import datetime,timedelta
+import random
+def points():
+
+	longhi, longlo, lathi, latlo = -79.313432,-79.437372 ,43.752594,43.634184
+	dlong = longhi -longlo
+	dlat =	lathi - latlo
+
+	for day in range(100):
+		for i in range(100):
+			lat = latlo + random.random()*dlat
+			long = longlo + random.random()*dlong
+			rating = round(max(1, min(random.gauss(day/20 + 1 , 0.75), 5)))
+
+			newrating = Rating(longitude = long, latitude= lat, rating = rating, date = datetime.now()-timedelta(days=100-day))
+			newrating.save()
+
+
+
+points()	
+
